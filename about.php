46bf1367ad81581e143985ae84e61300dfbf0d17
unknown <chavhanabhishek027@gmail.com>
2024-01-07 14:49:29 +0530

    updated code

---
+++ about.php
@@ -0,0 +1,89 @@
+<?php
+include("header.html");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!--Fonts CDN-->
+
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+  
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    
+
+    <!-- Font Awesome CDN -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <style>
+        *{
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+            font-family: poppins;
+        }
+     
+        
+    </style>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h1 style="text-align:center; margin-top:50px;"> About  </h1>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <p>Online voting systems are software platforms used to securely conduct votes. As a digital platform, they eliminate the need to cast your votes using paper or having to gather in person.
+ 
+                    They also protect the integrity of your vote by preventing voters from being able to vote multiple times.
+                     
+                    It is more convenient to use this system. </p>
+            </div>
+            
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h1 style="text-align:center; margin-top:50px;"> The Purpose of this platform </h1>
+            </div>
+            <div class="col-md-12">
+                <h3> Some of the purposes of this platform are as follows:-</h3>
+                
+              
+                </div>
+            <div class="col-md-12">
+            <ul>
+                        <li style="text-align:left; margin-left:20px;">maintains your vote’s integrity               </li>
+                        <li style="text-align:left; margin-left:20px;">provides an easy vote administrator experience</li>
+                        <li style="text-align:left; margin-left:20px;">creates a pleasant experience for your voters </li>
+                        <li style="text-align:left; margin-left:20px;">has mechanisms for adapting to unique votes  </li>
+                        <li style="text-align:left; margin-left:20px;">includes analytics, reporting, and auditing capabilities</li>
+             </ul>
+                    
+            </div>
+        </div>
+    </div>
+
+   
+
+        <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  
+    <script src="js/jquery-3.2.1.slim.min.js"></script>
+    <script src="js/popper.min.js"></script>    
+    <script src="js/bootstrap.min.js"></script>  
+</body>
+</html>
+<?php
+    include("footer.html");
+    ?>
